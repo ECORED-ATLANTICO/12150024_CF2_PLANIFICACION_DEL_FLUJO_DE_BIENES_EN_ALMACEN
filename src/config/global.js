4c9ea20437b9d2eec04d8613818205b9869f222e
AdonadoMolinares <donadomolinares@gmail.com>
2025-09-14 23:03:22 -0500

    commit

--- src/config/global.js
+++ src/config/global.js
@@ -36,13 +36,23 @@ export default {
       {
         nombreRuta: 'tema1',
         numero: '1',
-        titulo: 'Tema 1',
+        titulo: 'Manipulación de bienes',
         desarrolloContenidos: true,
         subMenu: [
           {
             numero: '1.1',
-            titulo: 'Subtema 1',
+            titulo: 'Concepto',
             hash: 't_1_1',
+          },
+          {
+            numero: '1.2',
+            titulo: 'Equipos de manipulación',
+            hash: 't_1_2',
+          },
+          {
+            numero: '1.3',
+            titulo: 'Normativa',
+            hash: 't_1_3',
           },
         ],
       },
@@ -50,20 +60,86 @@ export default {
       {
         nombreRuta: 'tema2',
         numero: '2',
-        titulo: 'Tema 2',
+        titulo: 'Organización de almacén',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '2.1',
+            titulo: 'Concepto',
+            hash: 't_2_1',
+          },
+          {
+            numero: '2.2',
+            titulo: 'Técnicas de organización',
+            hash: 't_2_2',
+          },
+          {
+            numero: '2.3',
+            titulo: 'Buenas prácticas',
+            hash: 't_2_3',
+          },
+        ],
       },
       {
         nombreRuta: 'tema3',
         numero: '3',
-        titulo: 'Tema 3',
+        titulo: 'Programación de almacén',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '3.1',
+            titulo: 'Concepto',
+            hash: 't_3_1',
+          },
+          {
+            numero: '3.2',
+            titulo: 'Tipos de programación',
+            hash: 't_3_2',
+          },
+          {
+            numero: '3.3',
+            titulo: 'Técnicas de cuantificación de recorridos',
+            hash: 't_3_3',
+          },
+          {
+            numero: '3.4',
+            titulo: 'TÓrdenes',
+            hash: 't_3_4',
+          },
+        ],
       },
       {
         nombreRuta: 'tema4',
         numero: '4',
-        titulo: 'Tema 4',
+        titulo: 'Planificación de almacén',
         desarrolloContenidos: true,
+        subMenu: [
+          {
+            numero: '4.1',
+            titulo: 'Concepto',
+            hash: 't_4_1',
+          },
+          {
+            numero: '4.2',
+            titulo: 'Técnicas de planificación',
+            hash: 't_4_2',
+          },
+          {
+            numero: '4.3',
+            titulo: 'Tipos de planes',
+            hash: 't_4_3',
+          },
+          {
+            numero: '4.4',
+            titulo: 'Herramientas tecnológicas',
+            hash: 't_4_4',
+          },
+          {
+            numero: '4.5',
+            titulo: 'Documentos',
+            hash: 't_4_5',
+          },
+        ],
       },
     ],
     subMenu: [
@@ -113,22 +189,131 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: 'Sitio web',
-      link: '',
+      tema: 'Manipulación de bienes',
+      referencia:
+        'Ecosistema de Recursos Educativos Digitales SENA. (2022). <i>Preparación de carga para distribución.</i> [Video]. YouTube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=6phzzMEfztk',
+    },
+    {
+      tema: 'Organización del almacén',
+      referencia:
+        'Soluciones Logísticas y Aduaneras. (2020). <i>Principales zonas de un almacén.</i> [Video]. YouTube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=sC_SAxJcexY&t=1s',
+    },
+    {
+      tema: 'Buenas prácticas',
+      referencia:
+        'Javier Peña. (2023). <i>8 consejos prácticos para Mejorar la operación del Almacén.</i> [Video]. YouTube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=uQvCZG3n6-o',
+    },
+    {
+      tema: 'Planificación de almacén',
+      referencia:
+        'Brain Logistic. (2022). <i>¿Qué es un Warehouse Management System?</i> [Video]. YouTube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=3HZ622AAcNw',
     },
   ],
   glosario: [
     {
-      termino: 'Término',
-      significado: 'Definición',
+      termino: 'Almacenamiento compacto',
+      significado:
+        'técnica que maximiza el espacio en almacenes, como sistemas <i>Drive-in</i> o <i>Push-back</i>, ideal para productos de baja rotación.',
+    },
+    {
+      termino: 'Almacenamiento vertical',
+      significado:
+        'uso de estanterías altas para aprovechar el espacio vertical del almacén.',
+    },
+    {
+      termino: 'ERP (<i>Enterprise Resource Planning</i>)',
+      significado:
+        'sistema integrado para gestionar procesos empresariales, como inventarios y finanzas.',
+    },
+    {
+      termino: 'FEFO (<i>First Expired, First Out</i>)',
+      significado:
+        'método de inventario donde los productos con fecha de caducidad más próxima se despachan primero.',
+    },
+    {
+      termino: 'FIFO (<i>First In, First Out</i>)',
+      significado:
+        'sistema de inventario donde los primeros productos en entrar son los primeros en salir.',
+    },
+    {
+      termino: 'Justo a tiempo (JIT)',
+      significado:
+        'estrategia que recibe productos solo cuando son necesarios, reduciendo inventarios.',
+    },
+    {
+      termino: '<i>Layout</i>',
+      significado:
+        'diseño físico de la distribución de espacios en un almacén.',
+    },
+    {
+      termino: 'LIFO (<i>Last In, First Out</i>)',
+      significado:
+        'método donde los últimos productos en entrar son los primeros en salir, usado en ciertos inventarios.',
+    },
+    {
+      termino: 'MTM (Métodos de Medición del Tiempo)',
+      significado:
+        'técnicas para analizar movimientos laborales y optimizar tiempos.',
+    },
+    {
+      termino: 'NTC (Norma Técnica Colombiana)',
+      significado:
+        'estándares técnicos aplicables en Colombia, como NTC 5611 para almacenamiento.',
+    },
+    {
+      termino: '<i>Put-away<i>',
+      significado:
+        'proceso de almacenamiento de mercancías recibidas en ubicaciones designadas.',
+    },
+    {
+      termino: 'SKU (<i>Stock Keeping Unit</i>)',
+      significado: 'código único que identifica cada producto en inventario.',
+    },
+    {
+      termino: '<i>Stock</i>',
+      significado: 'inventario de mercancías disponibles en el almacén.',
+    },
+    {
+      termino: '<i>Tracking</i>',
+      significado: 'monitoreo en tiempo real de mercancías o equipos.',
+    },
+    {
+      termino: '<i>Wearables</i>',
+      significado:
+        'dispositivos portátiles para registrar movimientos de operarios.',
     },
   ],
   referencias: [
     {
-      referencia: '',
-      link: '',
+      referencia:
+        'Algevasa Logistics. (2025). <i>La planificación en el almacén logístico.</i>',
+      link:
+        'https://www.algevasa.com/la-planificacion-en-el-almacen-logistico/ ',
+    },
+    {
+      referencia:
+        'AR Racking. (2024). <i>Cómo organizar un almacén de forma eficiente.</i>',
+      link:
+        'https://www.ar-racking.com/co/blog/consejos-para-organizar-un-almacen-de-forma-eficiente/',
+    },
+    {
+      referencia:
+        'Aula Centro de Formación. (2022). <i>Gestión de Almacén y Logística.</i>',
+      link:
+        'https://dl.dropboxusercontent.com/scl/fi/ucro9rrtkrv682y7s1qt6/Curso-Gesti-n-de-Almanc-n-y-Log-stica-Aula-Centro-Formaci-n.pdf?rlkey=azxypu2r27s5m21odlj9ymwys&dl=0',
+    },
+    {
+      referencia:
+        'Servicio Nacional de Aprendizaje (SENA). (2021). Zajuna Semilla. <i>121523_2_Virtual-Coordinación de procesos logísticos</i>',
+      link: 'https://zajuna.sena.edu.co/zajuna/course/view.php?id=32450',
     },
   ],
   creditos: [
@@ -136,14 +321,15 @@ export default {
       titulo: 'ECOSISTEMA DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Nombre completo',
-          cargo: 'Responsable del ecosistema',
+          nombre: 'Milady Tatiana Villamil Castellanos ',
+          cargo:
+            'Responsable del Ecosistema de Recursos Educativos Digitales (RED)',
           centro: 'Dirección General',
         },
         {
-          nombre: 'Nombre completo',
+          nombre: 'Miguel de Jesús Paredes Maestre',
           cargo: 'Responsable de línea de producción',
-          centro: 'Centro XYZ - Regional XYZ',
+          centro: 'Centro de Comercio y Servicios - Regional Atlántico',
         },
       ],
     },
@@ -151,9 +337,19 @@ export default {
       titulo: 'CONTENIDO INSTRUCCIONAL',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Nombre del rol',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'Yezid Arturo Choperena Guerrero',
+          cargo: 'Experto temático',
+          centro: 'Centro de Comercio y Servicios - Regional Atlántico',
+        },
+        {
+          nombre: 'Heydy Cristina González García',
+          cargo: 'Evaluadora instruccional',
+          centro: 'Centro de Comercio y Servicios - Regional Atlántico',
+        },
+        {
+          nombre: 'Jair Coll Gallardo',
+          cargo: 'Evaluador instruccional',
+          centro: 'Centro de Comercio y Servicios - Regional Atlántico',
         },
       ],
     },
@@ -161,19 +357,19 @@ export default {
       titulo: 'DISEÑO Y DESARROLLO DE RECURSOS EDUCATIVOS DIGITALES',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Diseñador de contenidos',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'Luis Gabriel Urueta ',
+          cargo: 'Diseñador web',
+          centro: 'Centro de Comercio y Servicios - Regional Atlántico',
         },
         {
-          nombre: 'Nombre responsable',
+          nombre: 'Alexander Donado Molinares',
           cargo: 'Desarrollador <i>full stack</i>',
-          centro: 'Centro XYZ - Regional XYZ',
+          centro: 'Centro de Comercio y Servicios - Regional Atlántico',
         },
         {
-          nombre: 'Nombre responsable',
+          nombre: 'Alexander Rafael Acosta Bedoya',
           cargo: 'Animador y productor audiovisual',
-          centro: 'Centro XYZ - Regional XYZ',
+          centro: 'Centro de Comercio y Servicios - Regional Atlántico',
         },
       ],
     },
@@ -181,14 +377,24 @@ export default {
       titulo: 'VALIDACIÓN RECURSO EDUCATIVO DIGITAL',
       autores: [
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Validador y vinculador de recursos educativos digitales',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'María Fernanda Morales Angulo',
+          cargo: 'Evaluadora de contenidos inclusivos y accesibles',
+          centro: 'Centro de Comercio y Servicios - Regional Atlántico',
         },
         {
-          nombre: 'Nombre responsable',
-          cargo: 'Evaluador de contenidos inclusivos y accesibles',
-          centro: 'Centro XYZ - Regional XYZ',
+          nombre: 'Luz Karime Amaya Cabra',
+          cargo: 'Evaluadora de contenidos inclusivos y accesibles',
+          centro: 'Centro de Comercio y Servicios - Regional Atlántico',
+        },
+        {
+          nombre: 'Jairo Luis Valencia Ebratt',
+          cargo: 'Validador y vinculador de recursos digitales',
+          centro: 'Centro de Comercio y Servicios - Regional Atlántico',
+        },
+        {
+          nombre: 'Jonathan Adié Villafañe',
+          cargo: 'Validador y vinculador de recursos digitales',
+          centro: 'Centro de Comercio y Servicios - Regional Atlántico',
         },
       ],
     },
--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -2,11 +2,25 @@
 .curso-main-container.pb-3
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
-    p Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. Fusce cursus mauris in nulla egestas vulputate. Phasellus viverra accumsan libero, sed luctus diam maximus euismod. Mauris sem risus, tincidunt vel libero in, lobortis commodo ante. Sed pulvinar luctus orci in rhoncus. Nulla ut dapibus risus, ac tempor est. Donec nulla augue, vehicula quis malesuada vitae, fermentum non nisi. Donec tincidunt nibh a magna bibendum rutrum.
-    .row.justify-content-center
-      .col-lg-10.mb-5.bgs.p-4.brad
+    .row.justify-content-center.align-items-center.mb-3
+      .col-12.col-lg-3.mb-3
         figure
-          img(src="@/assets/curso/sintesis.svg", alt="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu tincidunt erat. Fusce cursus mauris in nulla egestas vulputate. Phasellus viverra accumsan libero, sed luctus diam maximus euismod. Mauris sem risus, tincidunt vel libero in, lobortis commodo ante. Sed pulvinar luctus orci in rhoncus. Nulla ut dapibus risus, ac tempor est. Donec nulla augue, vehicula quis malesuada vitae, fermentum non nisi. Donec tincidunt nibh a magna bibendum rutrum. ")
+          img(src='@/assets/curso/sintesis/img_1.png', class="img-fluid")
+      .col-12.col-lg-9(data-aos='zoom-in-right')
+         p.mb-3 El componente formativo “Gestión operativa del flujo de bienes” permite comprender los procesos logísticos dentro de un almacén, desde su manipulación operativa hasta su planificación estratégica.
+         p.mb-0 Se inicia con el estudio de la manipulación de bienes, así como su normativa y los equipos requeridos. Posteriormente, se profundiza en la organización del almacén, lo que permite establecer su importancia en la optimización de los recursos y el flujo de materiales. Asimismo, se analizan los diferentes tipos de programación, proporcionando herramientas para la gestión de órdenes y la cuantificación de recorridos.
+    .row.justify-content-center.align-items-stard.mb-0
+      .col-12.col-lg-10.mb-0(data-aos="zoom-in-right")
+        .cajon.color-acento-contenido.p-4.mb-0
+          p.mb-0 Se presentan las técnicas de planificación que influyen en la eficiencia general, así como los distintos planes y herramientas tecnológicas de apoyo. La documentación aborda los formatos clave para el control y la trazabilidad. Para fortalecer la aplicación de estos conceptos, se detallan los aspectos prácticos sobre la manipulación segura y tácticos de la organización interna. Finalmente, se analiza la importancia de una correcta planificación para garantizar la efectividad de toda la operación logística.
+      .col-12.col-lg-2.mb-5
+        figure
+          img(src="@/assets/curso/sintesis/img_2.png", alt="")
+    
+    .row.justify-content-center
+      .col-12.col-lg-12.mb-5.bgs.p-4.brad
+        figure
+        img(src="@/assets/curso/sintesis/sintesis.svg", alt="El componente formativo “Gestión operativa del flujo de bienes” ofrece una aproximación integral a los conocimientos fundamentales sobre la manipulación de mercancías y la organización estratégica del almacén. Este módulo analiza detalladamente los procesos de manipulación de bienes, resaltando la selección y uso correcto de equipos en cuanto a eficiencia y cumplimiento normativo, y se desglosan las buenas prácticas para la disposición física de los productos. Asimismo, se estudian las metodologías para la programación de las operaciones del almacén. La propuesta profundiza en las técnicas para la cuantificación y optimización de recorridos, así como en la gestión de los diferentes tipos de órdenes de trabajo. Además, se destacan las estrategias y tipos de planes para la planificación del almacén, junto con el uso de herramientas tecnológicas de soporte, enfatizando la importancia de la correcta documentación para una toma de decisiones informada y la trazabilidad del flujo de bienes.")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/Sintesis.pdf')" target="_blank")
           .anexo__icono
